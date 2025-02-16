525879df9e15567ccb1213985fec8529430e23b0
cam <cjmooredev@gmail.com>
2025-02-16 12:30:47 -0400

    building out tables and relationships from Character models and data

--- scripts/load_characters.py
+++ scripts/load_characters.py
@@ -5,10 +5,49 @@ def load_character_data(input_file, session):
     try:
         with open(input_file, 'r') as f:
             data = json.load(f)
-        for _, char in data:
-            session.add(char)
-            # add data into tables here
+    
+        planets = ['LV-426', 'Fiorina 161', 'LV-223', 'Earth']
+
+        vessels = ['USM Auriga', 'the Betty', 'USCSS Prometheus', 'USS Sulaco', 'USCSS Nostromo', 'USCSS Patna']
+
+        films = ['Alien Resurrection', 'Prometheus', 'Alien 3', 'Alien', 'Aliens']
+
+        for planet_name in planets:
+            session.add(Planet(name=planet_name))
+
+        for vessel_name in vessels:
+            session.add(Vessel(name=vessel_name))
+
+        for film_name in films:
+            session.add(Film(name=film_name)) 
+
         session.commit()
+
+        for char in data:
+            character = Character(
+                name=char['name'],
+                rank=char['rank'],
+                affiliation=char['affiliation'],
+                species=char['species'],
+                height=char['height'],
+                hair=char['hair'],
+                eyecolor=char['eyecolor'],
+            )
+
+            for planet_name in char.get('planets',[]):
+                character.planets.append(planet_name)
+
+            for vessel_name in char.get('vessels',[]):
+                character.vessels.append(vessel_name)
+
+            for film_name in char.get('films',[]):
+                character.films.append(film_name)
+
+            session.add(character)
+
+        session.commit()
+        print(f"Successfully loaded {len(data)} characters to the db")
+
     except Exception as e:
         session.rollback()
         raise e
