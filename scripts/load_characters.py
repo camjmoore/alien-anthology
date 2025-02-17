f16ba055aa5be8432d5f090c419c93007f4f9952
cam <cjmooredev@gmail.com>
2025-02-17 05:24:51 -0400

    ensured planets, vessels films objects arels being passed

--- scripts/load_characters.py
+++ scripts/load_characters.py
@@ -5,21 +5,31 @@ def load_character_data(input_file, session):
     try:
         with open(input_file, 'r') as f:
             data = json.load(f)
-    
+
         planets = ['LV-426', 'Fiorina 161', 'LV-223', 'Earth']
 
         vessels = ['USM Auriga', 'the Betty', 'USCSS Prometheus', 'USS Sulaco', 'USCSS Nostromo', 'USCSS Patna']
 
         films = ['Alien Resurrection', 'Prometheus', 'Alien 3', 'Alien', 'Aliens']
 
+        planet_objects = {}
+        vessel_objects ={}
+        film_objects = {}
+
         for planet_name in planets:
-            session.add(Planet(name=planet_name))
+            planet = Planet(name=planet_name)
+            session.add(planet)
+            planet_objects[planet_name] = planet
 
         for vessel_name in vessels:
-            session.add(Vessel(name=vessel_name))
+            vessel = Vessel(name=vessel_name)
+            session.add(vessel)
+            vessel_objects[vessel_name] = vessel
 
         for film_name in films:
-            session.add(Film(name=film_name)) 
+            film = Film(name=film_name)
+            session.add(film) 
+            film_objects[film_name] = film
 
         session.commit()
 
@@ -35,13 +45,13 @@ def load_character_data(input_file, session):
             )
 
             for planet_name in char.get('planets',[]):
-                character.planets.append(planet_name)
+                character.planets.append(planet_objects[planet_name])
 
             for vessel_name in char.get('vessels',[]):
-                character.vessels.append(vessel_name)
+                character.vessels.append(vessel_objects[vessel_name])
 
             for film_name in char.get('films',[]):
-                character.films.append(film_name)
+                character.films.append(film_objects[film_name])
 
             session.add(character)
 
