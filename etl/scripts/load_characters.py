6f125bd4259c11edd57dc2ffa33f5e64194f188c
cam <cjmooredev@gmail.com>
2025-03-09 16:18:10 -0400

    refactored names for lists and dictionaries and implemented validation schemas

--- etl/scripts/load_characters.py
+++ etl/scripts/load_characters.py
@@ -1,60 +1,73 @@
 import json
 from shared.database.models import Character, Planet, Vessel, Film
+from shared.database.schemas import CharacterSchema, PlanetSchema, VesselSchema, FilmSchema
 
 def load_character_data(input_file, session):
     try:
         with open(input_file, 'r') as f:
             data = json.load(f)
+            #Character data schema validation
+            characters = [CharacterSchema(**char) for char in data]
 
-        planets = ['LV-426', 'Fiorina 161', 'LV-223', 'Earth']
+        planet_names = ['LV-426', 'Fiorina 161', 'LV-223', 'Earth']
 
-        vessels = ['USM Auriga', 'the Betty', 'USCSS Prometheus', 'USS Sulaco', 'USCSS Nostromo', 'USCSS Patna']
+        vessel_names = ['USM Auriga', 'the Betty', 'USCSS Prometheus', 'USS Sulaco', 'USCSS Nostromo', 'USCSS Patna']
 
-        films = ['Alien Resurrection', 'Prometheus', 'Alien 3', 'Alien', 'Aliens']
+        film_names = ['Alien Resurrection', 'Prometheus', 'Alien 3', 'Alien', 'Aliens']
 
+        #Planets, Vessels, Films data schema validation
+        planets = [PlanetSchema(name=p_name) for p_name in planet_names]
+        vessels = [VesselSchema(name=v_name) for v_name in vessel_names]
+        films = [FilmSchema(name=f_name) for f_name in film_names]
+
+        #Saved model instantiations
         planet_objects = {}
         vessel_objects ={}
         film_objects = {}
 
-        for planet_name in planets:
-            planet = Planet(name=planet_name)
+        #Instantiate models from validated data
+        for p in planets:
+            planet = Planet(name=p.name)
             session.add(planet)
-            planet_objects[planet_name] = planet
+            planet_objects[p.name] = planet
 
-        for vessel_name in vessels:
-            vessel = Vessel(name=vessel_name)
+        for v in vessels:
+            vessel = Vessel(name=v.name)
             session.add(vessel)
-            vessel_objects[vessel_name] = vessel
+            vessel_objects[v.name] = vessel
 
-        for film_name in films:
-            film = Film(name=film_name)
+        for f in films:
+            film = Film(name=f.name)
             session.add(film) 
-            film_objects[film_name] = film
+            film_objects[f.name] = film
 
+        #single session.commit more efficient than session.flush after every session.add
         session.commit()
 
-        for char in data:
+        #Instantiate Character models from validated data and populate relationships
+        for char in characters:
             character = Character(
-                name=char['name'],
-                rank=char['rank'],
-                affiliation=char['affiliation'],
-                species=char['species'],
-                height=char['height'],
-                hair=char['hair'],
-                eyecolor=char['eyecolor'],
+                name=char.name,
+                rank=char.rank,
+                affiliation=char.affiliation,
+                species=char.species,
+                height=char.height,
+                hair=char.hair,
+                eyecolor=char.eyecolor,
             )
 
-            for planet_name in char.get('planets',[]):
-                if planet_name in planet_objects:
-                    character.planets.append(planet_objects[planet_name])
+            #Append saved model instantiations to appropriate character instantiation
+            for name in char.planets:
+                if name in planet_objects:
+                    character.planets.append(planet_objects[name])
 
-            for vessel_name in char.get('vessels',[]):
-                if vessel_name in vessel_objects:
-                    character.vessels.append(vessel_objects[vessel_name])
+            for name in char.vessels:
+                if name in vessel_objects:
+                    character.vessels.append(vessel_objects[name])
 
-            for film_name in char.get('films',[]):
-                if film_name in film_objects:
-                    character.films.append(film_objects[film_name])
+            for name in char.films:
+                if name in film_objects:
+                    character.films.append(film_objects[name])
 
             session.add(character)
 
