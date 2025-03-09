9e2d8deec53bbbdfd4d229bf6002fe9a990f10ff
cam <cjmooredev@gmail.com>
2025-03-09 18:25:17 -0400

    building out API get endpoint

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from api.schemas import CharacterRead
+from shared.database.models import Character
+from shared.database.connection import create_session
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 
-app = FastAPI(
-    title="Alien Anthology API",
-    description="Serves data about the characters that inhabit the Alien Anthology universe",
-    version="1.0.0"
-)
+app = FastAPI()
 
-@app.get("/characters")
-def get_characters():
-    pass
+@app.get("/characters", response_model=List[CharacterRead])
+def read_characters(skip: int=0, limit: int=100, session: Session=Depends(create_session)):
+    characters = session.query(Character).offset(skip).limit(limit).all()
+    return characters
