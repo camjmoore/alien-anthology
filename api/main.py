3f859044e42177393cda321f5c027f84069f1ba6
cam <cjmooredev@gmail.com>
2025-03-09 22:55:13 -0400

    all character data is served

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from api.schemas import CharacterRead
 from shared.database.models import Character
-from shared.database.connection import create_session
+from shared.database.connection import get_session, initialize_db
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends
@@ -8,6 +8,6 @@ from fastapi import FastAPI, Depends
 app = FastAPI()
 
 @app.get("/characters", response_model=List[CharacterRead])
-def read_characters(skip: int=0, limit: int=100, session: Session=Depends(create_session)):
+def read_characters(skip: int=0, limit: int=100, session: Session=Depends(get_session)):
     characters = session.query(Character).offset(skip).limit(limit).all()
     return characters
